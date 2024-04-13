006ff4a628cc50a47694c289ba23e697e4a0f980
Iyanu <oyerindei13@gmail.com>
2024-04-14 00:08:49 +0100

    Update floatingLabel.tsx

--- src/components/floatingLabel.tsx
+++ src/components/floatingLabel.tsx
@@ -5,19 +5,18 @@ interface Props {
   value: string;
   type: string;
   name: string;
-  className: string;
   label: string;
   onChange: (event: ChangeEvent<HTMLInputElement>) => void;
 }
 
 const FloatingLabel: React.FC<Props> = (props) => {
   return (
-    <div className="form-group">
+    <div className="form-group-floating">
       <input
         value={props.value}
         type={props.type}
         name={props.name}
-        className={props.className}
+        className="form-control-floating"
         id="floating"
         placeholder=" "
         onChange={props.onChange}
